8f2a043ff903d939840ac53ac36f1aa1110d85ce
petrushandika <petrushandikasinaga@gmail.com>
2024-07-31 17:32:06 +0700

    feat: adding socket service

--- prisma/migrations/20240731103042_edit_prisma_schema/migration.sql
+++ prisma/migrations/20240731103042_edit_prisma_schema/migration.sql
@@ -5,7 +5,7 @@ CREATE TABLE "User" (
     "username" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT,
-    "googleId" TEXT NOT NULL,
+    "googleId" INTEGER NOT NULL,
     "diamond" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
